c26dc60fea843b73eb8b7ed880936295585b85b3
cristian muñoz <cristian muoz>
2017-02-07 18:02:50 -0500

    rip

--- server.js
+++ server.js
@@ -15,44 +15,47 @@ var mazeGenerator
     ySize = currentPackage.ySize ;
 
 
-    http.createServer(function (request, response) {
-       var queryData = url.parse(request.url, true).query;
-
-       xSize=queryData.x;
-       crear();
-      mazeGenerator.generate(spec);
-        response.writeHead(200, {"Content-Type":"text/plain"});
-        var row=[];
-        var  border="";
-      //response.write(String(mazeGenerator.printBoard()));
-      mazeGenerator.printBoard(function (rows,borders ) {
-        row=rows;
-        border=borders;
-
-      });
-      response.write(border+"\n");
-      for (var i = 0; i < row.length; i++) {
-          response.write(row[i]+"\n");
-      }
-
-        response.end();
-          //  mazeGenerator.printBoard();
-
-
-
-
-      }).listen(process.env.PORT||3000);
-
 
 
 ///////
 
 
 function crear() {
-  mazeGenerator = mazeFactory.create({ x: xSize, y: ySize });
+ mazeGenerator = mazeFactory.create({ x: xSize, y: ySize });
   let spec = {
       open: [
           { border: "N", list: [0,2,xSize-1] }
       ]
   };
 }
+
+
+
+
+http.createServer(function (request, response) {
+   var queryData = url.parse(request.url, true).query;
+
+   xSize=queryData.x;
+
+   mazeGenerator.generate(spec);
+    response.writeHead(200, {"Content-Type":"text/plain"});
+    var row=[];
+    var  border="";
+  //response.write(String(mazeGenerator.printBoard()));
+  mazeGenerator.printBoard(function (rows,borders ) {
+    row=rows;
+    border=borders;
+
+  });
+  response.write(border+"\n");
+  for (var i = 0; i < row.length; i++) {
+      response.write(row[i]+"\n");
+  }
+
+    response.end();
+      //  mazeGenerator.printBoard();
+
+
+
+
+  }).listen(process.env.PORT||3000);
